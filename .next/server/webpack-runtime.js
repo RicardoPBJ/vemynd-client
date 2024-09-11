bd6903933bbc895d4000a2e5879ac8491de3ea9d
RicardoPBJ <rbrumjunio@gmail.com>
2024-09-11 18:21:43 -0300

    Adicionando todos os elementos ao header

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("248f3f0cafbd02c4")
+/******/ 		__webpack_require__.h = () => ("c84ade313ce2c23a")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
@@ -163,6 +163,11 @@
 /******/ 			var r = fn();
 /******/ 			return r === undefined ? result : r;
 /******/ 		}
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	(() => {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/require chunk loading */
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,39 @@
 import Image from "next/image";
 import { logo, IconMenu } from "@/images";
 import { Button } from "./ui/button";
+import {
+  Sheet,
+  SheetTrigger,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetDescription,
+} from "@/components/ui/sheet";
 
 export default function Header() {
   return (
-    <div className="flex align-items-center justify-around">
-      <Image src={logo} alt="logo" className="w-[3rem] h-[2rem]" />
-      <Button className="bg-gradient-to-r from-[#5D47E9] to-[#C6BDFF] hover:bg-gradient-to-bl text-white font-bold rounded">
-        Cadastre-se agora
-      </Button>
-      <Image src={IconMenu} alt="iconMenu" />
-    </div>
+    <header className="flex items-center justify-around">
+      <Image src={logo} alt="logo" className="w-[10rem] h-[5rem]" />
+      <div className="flex justify-around">
+        <Button className="hidden lg:flex">Entrar</Button>
+        <Button className="bg-gradient-to-r from-[#5D47E9] to-[#C6BDFF] text-white font-bold">
+          Cadastre-se agora
+        </Button>
+        <Sheet>
+          <SheetTrigger>
+            <Image src={IconMenu} alt="iconMenu" className="lg:hidden" />
+          </SheetTrigger>
+          <SheetContent className="lg:hidden">
+            <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </header>
   );
 }
